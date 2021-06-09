1b4134c98613f20c1a840cae829b4d3760c9a50c
Vasantha Priya <66483465+vasantha3333@users.noreply.github.com>
2021-06-09 13:32:01 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+%%writefile App.py
+import streamlit as st
+from textblob import TextBlob
+from PIL import Image
+def main():
+  st.title("Sentiment Analysis")
+  img=Image.open("/content/sentiment-analysis_1.jpg")
+  st.image(img)
+  st.write("sentiment analysis")
+  text=st.text_input("Enter a sentence--")
+  if st.button("Analysis"):
+    br=TextBlob(text)
+    result=br.sentiment.polarity
+    if result==0:
+      st.success("This is a Neutral Message")
+    elif result>0:
+      st.success("This is a Positive Message")
+    else:
+      st.success("This is a Negative Message")
+   
+if __name__=="__main__":
+  main()
