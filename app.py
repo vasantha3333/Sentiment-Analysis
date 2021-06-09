33fdc8df344da2129b9d2c58c48e9420e4d31e4b
Vasantha Priya <66483465+vasantha3333@users.noreply.github.com>
2021-06-09 13:57:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from textblob import TextBlob
 from PIL import Image
 def main():
   st.title("Sentiment Analysis")
+  img=Image.open("sentiment-analysis_1.jpg")
+  st.image(img)
   st.write("sentiment analysis")
   text=st.text_input("Enter a sentence--")
   if st.button("Analysis"):
